ff4eaa57e4bd5fe9c7c61d2dc00a2057606ff689
Anakha Anie Jose <63898698+Anakha-Anie-Jose@users.noreply.github.com>
2022-06-15 06:23:51 +0530

    Add files via upload

--- exp3s.c
+++ exp3s.c
@@ -1,51 +1,45 @@
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#define PORT 8080
-int main(int argc, char const* argv[])
+#include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<sys/socket.h>
+void main()
 {
-    int sockfd, new_socket, valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Welcome";
- 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0))
-        == 0) {
-        printf("socket failed");
-        return -1;
-    }
- 
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("172.20.32.228");
-    address.sin_port = htons(PORT);
- 
-  
-    if (bind(sockfd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        printf("bind failed");
-        return -1;
-    }
-    if (listen(sockfd, 3) < 0) {
-        printf("listen");
-        return -1;
-    }
-    if ((new_socket
-         = accept(sockfd, (struct sockaddr*)&address,
-                  (socklen_t*)&addrlen))
-        < 0) {
-        printf("accept");
-        return -1;
-    }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Message sent to client\n");
-    return 0;
+int sockid,cllid,valread;
+char buf[1000];
+struct sockaddr_in server,client;
+sockid=socket(AF_INET,SOCK_STREAM,0);
+if(sockid<0)
+{
+printf("Error\n");
+return;
 }
+server.sin_family=AF_INET;
+server.sin_port=htons(8080);
+server.sin_addr.s_addr=INADDR_ANY;
+if(bind(sockid,(struct sockaddr*)&server,sizeof(server))<0)
+{
+printf("Bind  failed\n");
+return;
+}
+if(listen(sockid,3)<0)
+{
+printf("Listening Failed!!\n");
+return;
+}
+int addreslen=sizeof(server);
+if((cllid=accept(sockid,(struct sockaddr*)&server,(socklen_t*)&addreslen))<0)
+{
+printf("Accept Failed!!\n");
+return;
+}
+valread=read(cllid,buf,1000);
+printf("Message Received from client: %s\n",buf);
+char* msg="Hello, Server this side";
+send(cllid,msg,strlen(msg),0);
+printf("Message sent to client");
+}
+
+
+
+
